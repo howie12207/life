ce4747e945122c7b2b7e7e892d83f9011e7f1e98
Howie <howie12207@gmail.com>
2023-07-14 16:09:13 +0800

    feat: update

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
 
     return {
         plugins: [react()],
-        base: process.env.VITE_BASE_URL,
+        base: `${process.env.VITE_BASE_URL}/`,
         define: {
             __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
         },
