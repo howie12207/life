4c95bda263b3e092efd4831212528b9d80af5ab1
Howie <howie12207@gmail.com>
2023-08-08 16:18:33 +0800

    fix(build): add App.tsx handle

---
+++ src/AppMom.tsx
@@ -0,0 +1,102 @@
+import { useRef, useEffect, lazy, Suspense } from 'react';
+import {
+    createBrowserRouter,
+    RouterProvider,
+    useLocation,
+    useOutlet,
+    useNavigate,
+} from 'react-router-dom';
+import { CSSTransition, SwitchTransition } from 'react-transition-group';
+import Cookies from 'js-cookie';
+import { setNavigate } from '@/utils/navigateHelper';
+
+import { useAppDispatch } from '@/app/hook';
+import { updateToken } from '@/app/base';
+
+import Header from '@/components/header/Header';
+import Sidebar from '@/components/sidebar/Sidebar';
+import GoTop from '@/components/goTop/GoTop';
+import LoadingFull from '@/components/loadingFull/LoadingFull';
+
+const App = () => {
+    const dispatch = useAppDispatch();
+    useEffect(() => {
+        const token = Cookies.get('token');
+        if (token) dispatch(updateToken(token));
+    }, [dispatch]);
+
+    const Root = () => {
+        // navigate without hooks
+        const navigate = useNavigate();
+        setNavigate(navigate);
+
+        const location = useLocation();
+        const outlet = useOutlet();
+        const emptyRef = useRef(null);
+        const { nodeRef } = routes.find(route => route.path === location.pathname) ?? {
+            nodeRef: emptyRef,
+        };
+
+        useEffect(() => {
+            let pathname = location.pathname;
+            const articleIdReg = /^\/article\/.+$/;
+            if (articleIdReg.test(pathname)) pathname = '/article/:id';
+
+            const title = routes.find(route => route.path === pathname)?.title || '錯誤頁';
+            document.title = `${title} | Howie`;
+        }, [location.pathname]);
+
+        return (
+            <>
+                <Header />
+                <Sidebar />
+
+                <SwitchTransition>
+                    <CSSTransition
+                        key={location.pathname}
+                        nodeRef={nodeRef}
+                        timeout={300}
+                        classNames="page"
+                        unmountOnExit
+                    >
+                        <Suspense>
+                            <main ref={nodeRef}>{outlet}</main>
+                        </Suspense>
+                    </CSSTransition>
+                </SwitchTransition>
+
+                <GoTop />
+                <LoadingFull />
+            </>
+        );
+    };
+
+    const Home = lazy(() => import('@/pages/home'));
+    const Stock = lazy(() => import('@/pages/stock'));
+    const Error = lazy(() => import('@/pages/error'));
+
+    const routes = [
+        { path: '/', Component: Home, title: '首頁', nodeRef: useRef(null) },
+        {
+            path: '/stock',
+            Component: Stock,
+            title: '股票清單',
+            nodeRef: useRef(null),
+        },
+        { path: '*', Component: Error, nodeRef: useRef(null) },
+    ];
+    const router = createBrowserRouter(
+        [
+            {
+                path: '/',
+                Component: Root,
+                children: routes,
+            },
+        ],
+        { basename: import.meta.env.VITE_BASE_URL }
+    );
+
+    return <RouterProvider router={router} />;
+};
+
+export default App;
--- src/build/buildForMom.js
+++ src/build/buildForMom.js
@@ -1,5 +1,5 @@
 import { fileURLToPath } from 'url';
-import fs from 'fs';
+import { readFileSync, writeFileSync, unlinkSync, rename } from 'fs';
 import path, { dirname } from 'path';
 
 const rootPath = path.resolve(dirname(fileURLToPath(import.meta.url)), '../..');
@@ -7,34 +7,45 @@ let content = '';
 let targetPath = '';
 // .env.production
 targetPath = path.resolve(rootPath, '.env.production');
-content = fs.readFileSync(targetPath, 'utf-8');
+content = readFileSync(targetPath, 'utf-8');
 content = content.replace(/\/life/g, '/mom');
-fs.writeFileSync(targetPath, content);
+writeFileSync(targetPath, content);
 
 // deploy.sh  deploy.bat
 targetPath = path.resolve(rootPath, 'deploy.sh');
-content = fs.readFileSync(targetPath, 'utf-8');
+content = readFileSync(targetPath, 'utf-8');
 content = content.replace(/\/life/g, '/mom');
-fs.writeFileSync(targetPath, content);
+writeFileSync(targetPath, content);
 targetPath = path.resolve(rootPath, 'deploy.bat');
-content = fs.readFileSync(targetPath, 'utf-8');
+content = readFileSync(targetPath, 'utf-8');
 content = content.replace(/\/life/g, '/mom');
-fs.writeFileSync(targetPath, content);
+writeFileSync(targetPath, content);
 
 // 404.html
 targetPath = path.resolve(rootPath, '404.html');
-content = fs.readFileSync(targetPath, 'utf-8');
+content = readFileSync(targetPath, 'utf-8');
 content = content.replace(/\/life/g, '/mom');
-fs.writeFileSync(targetPath, content);
+writeFileSync(targetPath, content);
 
 // /api/stock.ts
 targetPath = path.resolve(rootPath, 'src/api/stock.ts');
-content = fs.readFileSync(targetPath, 'utf-8');
+content = readFileSync(targetPath, 'utf-8');
 content = content.replace(/apiPath = 'stock'/, "apiPath = 'stock2'");
-fs.writeFileSync(targetPath, content);
+writeFileSync(targetPath, content);
 
 // /config/constant.ts
 targetPath = path.resolve(rootPath, 'src/config/constant.ts');
-content = fs.readFileSync(targetPath, 'utf-8');
+content = readFileSync(targetPath, 'utf-8');
 content = content.replace(/FEE_RATE = 0.001425 \* 0.28/, 'FEE_RATE = 0.001425 * 0.6');
-fs.writeFileSync(targetPath, content);
+writeFileSync(targetPath, content);
+
+// /App.ts
+targetPath = path.resolve(rootPath, 'src/App.tsx');
+const originTargetPath = path.resolve(rootPath, 'src/AppMom.tsx');
+rename(originTargetPath, targetPath, err => {
+    if (err) {
+        console.log('Error rename');
+    } else {
+        console.log('Successfully renamed the file!');
+    }
+});
