0b8f0c9cd001d10148bff0c6aed683fe58e7e93a
Howie <howie12207@gmail.com>
2023-11-22 15:37:17 +0800

    feat: add hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,5 @@
 import { default as useIntersectionObserver } from './useIntersectionObserver';
+import { useIsFirstRender } from './useIsFirstRender';
+import { useUpdateEffect } from './useUpdateEffect';
 
-export { useIntersectionObserver };
+export { useIntersectionObserver, useIsFirstRender, useUpdateEffect };
